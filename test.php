817af886d4e86bb71bffe773295cf21d5d2c8c8e
Olle Harstedt <olleharstedt@yahoo.com>
2015-07-18 01:06:14 +0200

    [WIP] Adapt type-inference for smallest possible subset of PHP

--- test.php
+++ test.php
@@ -1,3 +1,11 @@
 <?php
 
 $a = 10;
+
+/*
+[(Ast.Stmt
+    (Ast.Expr
+       (<opaque>,
+        Ast.Binop ((Ast.Eq None), (<opaque>, (Ast.Lvar (<opaque>, "$a"))),
+          (<opaque>, (Ast.Int (<opaque>, "10")))))))]
+ */
--- type.ml
+++ type.ml
@@ -25,7 +25,12 @@ type term =
 (* Infix function composition *)
 let (@@) f g x = f (g x)
 
-type typ = Var of string | Number | Bool | Arrow of typ * typ
+type typ =
+  Var of string
+| Number (* We can't infer if number is int or float from pure PHP *)
+| Unit  (* All statements return this, and functions without return *)
+| Bool
+| Arrow of typ * typ
 [@@deriving show]
 
 (*  A substitution is just a function typ -> typ.  *)
@@ -52,7 +57,6 @@ exception Circularity
 exception Mismatch
 
 (*  unify : typ * typ -> (typ -> typ)  *)
-
 let rec unify (a : typ) t = match a, t with
 | (Var a, t) ->
       if Var a = t then identity
@@ -61,11 +65,15 @@ let rec unify (a : typ) t = match a, t with
 | (t, Var a) -> unify (Var a) t
 | (Number, Number) -> identity
 | (Bool, Bool) -> identity
+| (Unit, Unit) -> identity
 | (Arrow(t1, t2), Arrow(t3, t4)) ->
       let s1 = unify t1 t3 in
       let s2 = unify (s1 t2) (s1 t4) in
       s2 @@ s1
-| (_, _) -> raise Mismatch
+| (a, b) -> 
+    print_endline (show_typ a);
+    print_endline (show_typ b);
+    raise Mismatch
 
 (*  An environment is a function string -> typ.
  *  I've included code for the empty environment
@@ -74,14 +82,14 @@ let rec unify (a : typ) t = match a, t with
 
 exception UnboundID
 
+type env = string -> typ
+
 let emptyenv (x : string) : typ = raise UnboundID
 
 (*  update : (string -> typ) -> string -> typ -> string -> typ  *)
-
 let update e (x : string) (ty : typ) y = if x = y then ty else e y
 
 (*  New type variable generator:  newtypevar () and reset ()  *)
-
 let letters = ["a";"b";"c";"d";"e";"f";"g";"h";"i";"j";"k";"l";"m"; "n";"o";"p";"q";"r";"s";"t";"u";"v";"w";"x";"y";"z"]
 let cnt = ref 0
 let typevars = ref letters
@@ -99,82 +107,47 @@ let reset () = (
   cnt := 0;
   typevars := letters
 )
-(*  Milner's algorithm W : (string -> typ) * term -> (typ -> typ) * typ *)
 
-let rec w env t = match env, t with
-| (env, AST_ID x) ->
-      (* E(x) = t
-         ----------
-         E |- x : t *)
-      (identity, env x)
+let rec w_stmt (env : env) (term : stmt) = match env, term with
+| env, Expr (_, expr) ->
+    let (s1, t1) = w_expr_ env expr in
+    (s1 @@ env, Unit)
+| _ -> failwith "Not implemented: stmt"
 
-| (env, AST_NUM _)   ->
-      (* E |- n : int *)
-      (identity, Number)
+and w_def (env : env) (term : def) = match env, term with
+| env, Stmt stmt ->
+    let (s1, t1) = w_stmt env stmt in
+    let s2 = unify t1 Unit in
+    (s2 @@ s1, Unit)
+| _ -> failwith "Not implemented: def"
 
-| (env, AST_BOOL _) ->
-      (* E |- true : bool
-         E |- false : bool *)
-      (identity, Bool)
-
-| (env, AST_SUCC) ->
-      (* E |- succ : int -> int *)
-      (identity, Arrow (Number, Number))
-
-| (env, AST_PRED) ->
-      (* E |- pred : int -> int *)
-      (identity, Arrow (Number, Number))
-
-| (env, AST_ISZERO) ->
-      (* E |- iszero : int -> bool *)
-      (identity, Arrow (Number, Bool))
-
-| (env, AST_IF (e1, e2, e3)) ->
-      (* E |- e1 : bool  E |- e2 : t  E |- e3 : t
-         ----------------------------------------
-         E |- if e1 then e2 else e3 : t           *)
-      let (s1, t1) = w env e1 in
-      let s2 = unify t1 Bool in
-      let (s3, t2) = w (s2 @@ s1 @@ env) e2 in
-      let (s4, t3) = w (s3 @@ s2 @@ s1 @@ env) e3 in
-      let s5 = unify (s4 t2) t3 in
-        (s5 @@ s4 @@ s3 @@ s2 @@ s1, s5 t3)
-
-| (env, AST_FUN (x, e)) ->
-      (* E[x : t1] |- e : t2
-         -------------------------
-         E |- fn x => e : t1 -> t2 *)
-      let t1 = newtypevar() in
-      let (s, t2) = w (update env x t1) e in
-        (s, s (Arrow (t1, t2)))
-
-| (env, AST_APP (e1, e2)) ->
-      (* E |- e1 : t1 -> t2  E |- e2 : t1
-         --------------------------------
-         E |- e1 e2 : t2                  *)
-      let (s1, t1) = w env e2 in
-      let t2 = newtypevar() in
-      let (s2, t3) = w (s1 @@ env) e1 in
-      let s3 = unify (s2 t3) (Arrow ((s2 @@ s1) t1, t2)) in
-      let s = s3 @@ s2 @@ s1 in
-        (s, s t2)
-
-| (env, AST_REC (x, e)) ->
-      (* E[x : t] |- e : t
-         -------------------
-         E |- rec x => e : t *)
-      let t = newtypevar() in
-      let (s1, te) = w (update env x t) e in
-      let s2 = unify (s1 t) (s1 te) in
-      let s = s2 @@ s1 in
-        (s, s t)
-
-| (env, AST_ERROR _) -> raise Mismatch (* shouldn't happen *)
-
-(*  Put a type in canonical form, such that the type variables start
- *  from scratch.
+(**
+ * Milner's algorithm W : (string -> typ) * term -> (typ -> typ) * typ
+ *
+ * @param env string -> typ
+ * @param term Ast
  *)
+and w_expr_ (env : env) (term : expr_) = match env, term with
+| env, Lvar (_, x) ->
+    (identity, env x)
+| (env, Binop (op, (_, exp1), (_, exp2))) ->
+    (
+      match op with
+      | Eq None ->
+        let (s1, t1) = w_expr_ env exp2 in
+        let (s2, t2) = w_expr_ (s1 @@ env) exp2 in
+        let s = s1 @@ s2 in
+        (s, s t2)
+      | _ -> failwith "Not implemented: op"
+    )
+| (env, Int (_, pstr)) ->
+    (identity, Number)
+| (env, _) -> failwith "Not implemented: expr_"
 
+(**  
+ * Put a type in canonical form, such that the type variables start
+ * from scratch.
+ *)
 let rec canonical' env t = match env, t with
 | (env, Var v) ->
     begin
@@ -189,72 +162,23 @@ let rec canonical' env t = match env, t with
 | (env, x) -> (env, x)
 
 let canonical t =
-  (reset ();
-  let (_, t) = canonical' emptyenv t
-  in
-    t
-  )
+  reset ();
+  let (_, t) = canonical' emptyenv t in
+  t
 
-(*  Here's a driver program that uses W to find the principal type of e
- *  and then displays it nicely.
+(**  
+ * Here's a driver program that uses W to find the principal type of e
+ * and then displays it nicely.
  *)
-
-let infer e =
-  let (s, t) = (reset (); w emptyenv e)
-  in
-    print_endline ("The principal type is " ^ show_typ (canonical t) ^ "\n")
+let infer exp =
+  let (s, t) = (reset (); w_def emptyenv exp) in
+  let typ = canonical t in
+  print_endline ("The principal type is " ^ show_typ typ)
 
 let _ =
-  infer (AST_NUM 10)
-
-(*
-(* test for // wrongly independent substitutions in AST_APP *)
-infer (parsestr "fn f => fn g => g (f true) (f 1)");
-exception Mismatch
-
-(* test for wrong ordering of substitutions in AST_REC *)
-infer (parsestr "rec f => rec g => fn x => f (g x)");
-'a -> 'a
-
-infer (parsestr "fn f => fn x => f (f x)");
-('a -> 'a) -> 'a -> 'a
-
-infer (parsestr "fn f => fn g => fn x => f (g x)");
-('a -> 'b) -> ('c -> 'a) -> 'c -> 'b
-
-infer (parsestr "fn b => if b then 1 else 0");
-bool -> int
-
-infer (parsestr "rec f => fn b => if b then 1 else f true");
-bool -> int
-
-infer (parsestr "rec f => fn x => f x");
-'a -> 'b
-
-infer (parsestr "rec f => rec g => fn x => f (f x)");
-'a -> 'a
-
-infer (parsestr "rec m => fn x => fn y => if iszero y then x else m (pred x) (pred y)");
-int -> int -> int
-
-infer (parsestr "rec even => fn n => if iszero n then true else if iszero (pred n) then false else even (pred (pred n))");
-int -> bool
-
-infer (parsefile "twice.pcf");
-exception Circularity
-
-infer (parsefile "minus.pcf");
-int
-
-infer (parsefile "factorial.pcf");
-int
-
-infer (parsefile "fibonacci.pcf");
-int
-
-infer (parsefile "lists.pcf");
-exception Mismatch
-
-infer (parsefile "not.pcf");
-exception Mismatch
-*)
+  infer 
+    (Ast.Stmt
+        (Ast.Expr
+           (Pos.none,
+            Ast.Binop ((Ast.Eq None), (Pos.none, (Ast.Lvar (Pos.none, "$a"))),
+              (Pos.none, (Ast.Int (Pos.none, "10")))))))
