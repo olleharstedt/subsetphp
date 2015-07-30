70122b4f6e35d4a5e8a1d15b90891f0d75238bec
Olle Harstedt <olleharstedt@yahoo.com>
2015-07-30 21:53:43 +0200

    Add typed AST file

---
+++ typedast.ml
@@ -0,0 +1,26 @@
+(**
+ * AST built up by type-inference, with types.
+ * Used to generate LLVM IR.
+ *
+ * @author Olle Harstedt
+ * @since 2015-07-29
+ *)
+
+type ty =
+  | TNumber
+  | TString
+  | TUnit
+
+type id = Pos.t * string
+
+type program = def list
+
+and def =
+  | Stmt of stmt
+
+and stmt =
+  | Expr of expr
+
+and expr = Pos.t * expr_
+and expr_ =
+  | Id of id * ty  (* Added type here *)
