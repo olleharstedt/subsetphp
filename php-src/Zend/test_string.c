afd1463e1d232de5610ff726c74fe09d5faabe49
Olle Harstedt <olleharstedt@yahoo.com>
2015-09-04 21:00:03 +0200

    Disallow case for concatenation where result equals first str

--- php-src/Zend/test_string.c
+++ php-src/Zend/test_string.c
@@ -20,6 +20,9 @@ struct _zend_string {
 int subsetphp_concat_function(zend_string *result, zend_string *str1, zend_string *str2) /* {{{ */
 {
 
+  // Disallow this for now (pointer stuff)
+  assert(result != str1);
+
   size_t str1_len = str1->len;
   size_t str2_len = str2->len;
   size_t result_len = str1_len + str2_len;
@@ -28,16 +31,17 @@ int subsetphp_concat_function(zend_string *result, zend_string *str1, zend_strin
     zend_error_noreturn(E_ERROR, "String size overflow");
   }
 
-  if (result == str1) { // && Z_REFCOUNTED_P(result)) {
+  //if (result == str1) { // && Z_REFCOUNTED_P(result)) {
     /* special case, perform operations on result */
-    result = zend_string_realloc(result, result_len, 1);
-  } else {
+    //result = zend_string_realloc(str1, result_len, 1);
+  //} else {
     memcpy(result->val, str1->val, str1_len);
-  }
+  //}
 
   memcpy(result->val + str1_len, str2->val, str2_len);
   result->len = result_len;
   result->val[result_len] = '\0';
+  printf("subsetphp_concat_function: %s\n", result->val);
 
 	return SUCCESS;
 }
@@ -50,8 +54,7 @@ void test(zend_string *str) {
 int main(void) {
 
   // Alloc init
-  
-  
+
 	zend_startup_constants();
   zend_interned_strings_init();
   zend_string *str = zend_string_init("asd", 3, 1);
@@ -63,7 +66,6 @@ int main(void) {
   printf("%s\n", str->val);
 
   zend_string *str2 = zend_string_init("qwe", 3, 1);
-  //zend_string *result; 
   zend_string *result = zend_string_init("", 0, 1);
   subsetphp_concat_function(result, str, str2);
   printf("%s\n", result->val);
