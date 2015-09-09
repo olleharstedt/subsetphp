819896e8532c1bfc6196b9b3839e69f4adee5f37
Olle Harstedt <olleharstedt@yahoo.com>
2015-09-10 01:04:38 +0200

    Add stubs for wrapping zend_string in OCaml value

--- bindings.c
+++ bindings.c
@@ -3,6 +3,35 @@
 #include <zend_types.h>
 #include <zend_string.h>
 
+#include "caml/alloc.h"
+#include "caml/compact.h"
+#include "caml/custom.h"
+#include "caml/finalise.h"
+#include "caml/freelist.h"
+#include "caml/gc.h"
+#include "caml/gc_ctrl.h"
+#include "caml/major_gc.h"
+#include "caml/minor_gc.h"
+#include "caml/misc.h"
+#include "caml/mlvalues.h"
+#include "caml/sys.h"
+#include "caml/startup_aux.h"
+#ifdef NATIVE_CODE
+#include "stack.h"
+#else
+#include "caml/stacks.h"
+#endif
+
+/* Declaration of variables used in the asm code */
+extern char * caml_top_of_stack;
+extern char * caml_bottom_of_stack;
+extern uintnat caml_last_return_address;
+extern value * caml_gc_regs;
+extern char * caml_exception_pointer;
+extern value caml_globals[];
+extern intnat caml_globals_inited;
+extern intnat * caml_frametable[];
+
 extern double printd(double x) {
   printf("%f\n", x);
   return 0;
@@ -11,6 +40,59 @@ extern double printd(double x) {
 extern double prints(zend_string* str) {
   printf("%s\n", str->val);
   return 0;
+}
+
+/* String functions */
+
+/* Code below from OCaml C API documentation */
+
+/* Encapsulation of zend_string
+   as OCaml custom blocks. */
+
+static struct custom_operations subsetphp_zend_string = {
+  "subsetphp.zend_string",
+  custom_finalize_default,
+  custom_compare_default,
+  custom_hash_default,
+  custom_serialize_default,
+  custom_deserialize_default
+};
+
+
+/* Accessing the zend_string* part of an OCaml custom block */
+#define Window_val(v) (*((zend_string **) Data_custom_val(v)))
+
+extern value subsetphp_string_alloc(size_t len, int persistent)
+{
+	zend_string *ret = (zend_string *)pemalloc(ZEND_MM_ALIGNED_SIZE(_STR_HEADER_SIZE + len + 1), persistent);
+
+	GC_REFCOUNT(ret) = 1;
+#if 1
+	/* optimized single assignment */
+	GC_TYPE_INFO(ret) = IS_STRING | ((persistent ? IS_STR_PERSISTENT : 0) << 8);
+#else
+	GC_TYPE(ret) = IS_STRING;
+	GC_FLAGS(ret) = (persistent ? IS_STR_PERSISTENT : 0);
+	GC_INFO(ret) = 0;
+#endif
+	ret->h = 0;
+	ret->len = len;
+	return ret;
+}
+
+/**
+ * All alloc functions must be changed to play nicely with
+ * the OCaml GC
+ *
+ * @return value
+ */ 
+extern value subsetphp_string_init(const char *str, size_t len, int persistent)
+{
+	zend_string *ret = zend_string_alloc(len, persistent);
+
+	memcpy(ret->val, str, len);
+	ret->val[len] = '\0';
+	return ret;
 }
 
 /**
@@ -39,3 +121,15 @@ extern zend_string* subsetphp_concat_function(zend_string *str1, zend_string *st
 
 	return result;
 }
+
+/**
+ * Init the OCaml GC
+ */
+extern void subsetphp_gc_init() {
+  caml_parse_ocamlrunparam();
+  caml_init_gc (caml_init_minor_heap_wsz, caml_init_heap_wsz,
+                caml_init_heap_chunk_sz, caml_init_percent_free,
+                caml_init_max_percent_free);
+  //caml_init_stack (caml_init_max_stack_wsz);
+}
+
