7d67abeec4a5d89ce107a42587579c656f3f6d25
Olle Harstedt <olleharstedt@yahoo.com>
2016-03-18 21:47:39 +0100

    Read filename from command-line

--- llvm_test.ml
+++ llvm_test.ml
@@ -1168,11 +1168,17 @@ let _ =
   let _ = build_ret (const_int i32_t 0) llbuilder in
   *)
 
+  (* Get command-line argument file *)
+  let filename = if Array.length Sys.argv = 2 then
+    Sys.argv.(1)
+  else
+    "test.php"
+  in
 
   let open Parser_hack in
   SharedMem.(init default_config);
-  let file_content = Utils.read_file "test.php" in
-  let parser_return = Parser_hack.program (Relative_path.Root, "test.php") file_content in
+  let file_content = Utils.read_file filename in
+  let parser_return = Parser_hack.program (Relative_path.Root, filename) file_content in
 
   (* If no error, dump a lot of info *)
   if parser_return.error = None then begin
