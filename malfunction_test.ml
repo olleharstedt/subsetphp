394ae1fb203b1209f024b4df70c1a75e3593641a
Olle Harstedt <olleharstedt@yahoo.com>
2016-07-27 22:58:53 +0200

    Trying to test let bindings in malfunction

--- malfunction_test.ml
+++ malfunction_test.ml
@@ -14,10 +14,16 @@
  * @author Olle Harstedt
  *)
 
+(*
+ * `autocmd WinLeave * if exists('b:terminal_job_id') | let g:last_term_id = b:terminal_job_id`, and `autocmd BufWrite <buffer> call jobsend(b:terminal_job_id, "make\n")`
+ *)
+(*:autocmd! BufWrite * call jobsend(70, "clear; make malf; ./malf\n") *)
+
 open Typedast
 open Malfunction
 open Malfunction_sexp
 open Lexing
+
 
 (*
 (module
@@ -47,14 +53,46 @@ let test_code =
     ])
   ]))
 
+(*
+$myVar = 10;
+echo 10;
+ *)
+let basic_code =
+  (dum, List ([
+    (dum, Atom "module");
+      (dum, List [
+        (dum, Atom "let");
+        (dum, List [
+          (dum, Var "myVar");
+          (dum, Int 10);
+        ]);
+        (dum, List [
+          (dum, Atom "_");
+          (dum, List [
+              (dum, Atom "apply");
+              (dum, List [
+                  (dum, Atom "global");
+                  (dum, Var "Pervasives");
+                  (dum, Var "print_string")
+              ]);
+              (dum, String "Hello, world!\n");
+          ])
+        ]);
+      ]);
+    (dum, List [
+      (dum, Atom "export");
+    ])
+  ]))
+
 let _ =
   (*
   let options = [`Shared] in
   let lm = Malfunction_compiler.module_to_lambda ~options test_code in
   *)
-  Malfunction_sexp.print Format.str_formatter test_code;
+  Malfunction_sexp.print Format.str_formatter basic_code;
   let str = Format.flush_str_formatter () in
-  let e = Malfunction_parser.parse_mod test_code in
+  print_endline str;
+  let e = Malfunction_parser.parse_mod basic_code in
   let l = Malfunction_compiler.module_to_lambda e in
   let c = Malfunction_compiler.lambda_to_cmx "malf_file" "malf_output" l in
-  print_endline str
+  ()
