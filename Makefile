06a2c8d9d6c055094635da63b9af5667f29ba24c
Olle Harstedt <olleharstedt@yahoo.com>
2016-07-28 22:36:20 +0200

    Add malfunction test for ref and double

--- Makefile
+++ Makefile
@@ -166,7 +166,8 @@ runtime2.o: bindings2.c semigc
 llvm_test: runtime2.o subsetphp typedast.cmx llvm_test.ml
 	ocamlfind ocamlopt -g -w @5 -cc g++ -cc -lncurses -cclib -lffi -I ~/.opam/4.02.1/llvm/ -cc g++ -package ppx_deriving.show,llvm,llvm.bitreader,llvm.bitwriter,llvm.target,llvm.analysis,llvm.scalar_opts,llvm.linker -linkpkg -linkall fbident.cmx utils.cmx str.cmxa sys_utils.cmx fbpath.cmx relative_path.cmx pos.cmx errors.cmx lexer_hack.cmx namespace_env.cmx lint.cmx prefix.cmx eventLogger.cmx realpath.o hh_shared.o sharedMem.cmx parser_heap.cmx namespaces.cmx parser_hack.cmx fileInfo.cmx ast.cmx typedast.cmx infer.cmx php-src/Zend/*.o llvm_test.ml -o llvm_test
 
-malf: malfunction_test.ml typedast.cmx pos.cmx realpath.o hh_shared.o
+
+malf: typedast.cmx pos.cmx realpath.o hh_shared.o malfunction_test.ml
 	ocamlfind ocamlopt -g -package compiler-libs.common,ppx_deriving,dynlink,unix,malfunction -linkpkg -linkall -I ~/.opam/4.03.0+flambda/lib/malfunction -I ~/.opam/4.03.0+flambda/lib fbident.cmx utils.cmx str.cmxa sys_utils.cmx fbpath.cmx relative_path.cmx pos.cmx typedast.cmx realpath.o hh_shared.o malfunction_test.ml -o malf
 
 comp: llvm_test runtime2.o semigc
--- malfunction_test.ml
+++ malfunction_test.ml
@@ -15,15 +15,27 @@
  *)
 
 (*
+21:07:14 - mrvn: a ref is just a block with 1 value.
+21:07:42 - ollehar: with value 1, or with one value?
+21:07:52 - mrvn: one
+21:07:56 - ollehar: ok, thanks
+21:08:48 - mrvn: ref 1 is a block containing 3 (1 << 1 + tag), ref 1.0 is a block containing a pointer to the float block.
+21:09:10 - mrvn: unless thats optimized like float array
+21:09:30 - ollehar: what did that 3 come from?
+21:09:42 - mrvn: Val_int(1)
+21:10:11 - ollehar: ok
+22:06:51 - mrvn: # type 'a ref = { mutable contents: 'a };;
+ *)
+
+(*
  * `autocmd WinLeave * if exists('b:terminal_job_id') | let g:last_term_id = b:terminal_job_id`, and `autocmd BufWrite <buffer> call jobsend(b:terminal_job_id, "make\n")`
  *)
-(*:autocmd! BufWrite * call jobsend(70, "clear; make malf; ./malf\n") *)
+(*:autocmd! BufWritePost * call jobsend(1, "clear; make malf; ./malf\n") *)
 
 open Typedast
 open Malfunction
 open Malfunction_sexp
 open Lexing
-
 
 (*
 (module
@@ -84,15 +96,127 @@ let basic_code =
     ])
   ]))
 
+(**
+ * $a = 10
+ * $a = 5
+ * echo $a
+
+(module
+  (_
+    (let ($a (block (tag 0) 10)) ($a (block (tag 0) 5))
+      (apply (global $Pervasives $print_int) (field 0 $a))))
+  (export))
+
+ *)
+let ref_var_code = 
+  (dum, List ([
+    (dum, Atom "module");
+    (dum, List [
+      (dum, Atom "_");
+      (dum, List [
+        (dum, Atom "let");
+        (dum, List [
+          (dum, Var "a");
+          (dum, List [
+            (dum, Atom "block");
+            (dum, List [
+              (dum, Atom "tag");
+              (dum, Int 0)
+            ]);
+            (dum, Int 10)
+          ]);
+        ]);
+        (dum, List [
+          (dum, Var "a");
+          (dum, List [
+            (dum, Atom "block");
+            (dum, List [
+              (dum, Atom "tag");
+              (dum, Int 0)
+            ]);
+            (dum, Int 5)
+          ]);
+        ]);
+        (dum, List [
+          (dum, Atom "apply");
+          (dum, List [
+              (dum, Atom "global");
+              (dum, Var "Pervasives");
+              (dum, Var "print_int")
+          ]);
+          (dum, List [
+            (dum, Atom "field");
+            (dum, Int 0);
+            (dum, Var "a");
+          ]);
+        ]);
+      ]);
+    ]);
+    (dum, List [
+      (dum, Atom "export");
+    ])
+  ]))
+
+(*
+ * $a = 1.0
+ * echo $a
+ *)
+let double_code =
+  (dum, List ([
+    (dum, Atom "module");
+    (dum, List [
+      (dum, Atom "_");
+      (dum, List [
+        (dum, Atom "let");
+        (dum, List [
+          (dum, Var "a");
+          (dum, List [
+            (dum, Atom "block");
+            (dum, List [
+              (dum, Atom "tag");
+              (dum, Int 0)
+            ]);
+            (dum, List [
+              (dum, Atom "block");
+              (dum, List [
+                (dum, Atom "tag");
+                (dum, Int Obj.double_tag)
+              ]);
+              (dum, String "1.0")
+            ]);
+          ]);
+        ]);
+        (dum, List [
+          (dum, Atom "apply");
+          (dum, List [
+              (dum, Atom "global");
+              (dum, Var "Pervasives");
+              (dum, Var "print_float")
+          ]);
+          (dum, List [
+            (dum, Atom "field");
+            (dum, Int 0);
+            (dum, Var "a");
+          ]);
+        ]);
+      ]);
+    ]);
+    (dum, List [
+      (dum, Atom "export");
+    ])
+  ]))
+
 let _ =
   (*
   let options = [`Shared] in
   let lm = Malfunction_compiler.module_to_lambda ~options test_code in
   *)
-  Malfunction_sexp.print Format.str_formatter basic_code;
+
+  Malfunction_sexp.print Format.str_formatter double_code;
   let str = Format.flush_str_formatter () in
   print_endline str;
-  let e = Malfunction_parser.parse_mod basic_code in
+  let e = Malfunction_parser.parse_mod double_code in
   let l = Malfunction_compiler.module_to_lambda e in
   let c = Malfunction_compiler.lambda_to_cmx "malf_file" "malf_output" l in
+
   ()
